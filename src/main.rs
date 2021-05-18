6e28d29d880e32045412a41e4874c4460f3a6f4a
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-18 17:35:13 -0400

    Basic gravity

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use std::collections::HashSet;
+
 use bevy::prelude::*;
 use itertools::Itertools;
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
-pub struct SnakeMovement;
+pub struct SnakeMovementLabel;
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
 pub struct TransformLabel;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
+pub struct GravityLabel;
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 struct GridLocation {
@@ -23,24 +28,33 @@ impl std::ops::Add for GridLocation {
     }
 }
 
+struct Snake;
+
+struct Ground;
+
 struct SnakeParts(Vec<Entity>);
 
 const GRID_WIDTH: f32 = 16.0;
 const GRID_HEIGHT: f32 = 16.0;
 
 fn main() {
-    println!("Hello, world!");
     App::build()
         .add_plugins(DefaultPlugins)
         .insert_resource(SnakeParts(vec![]))
         .add_system(bevy::input::system::exit_on_esc_system.system())
         .add_startup_system(setup.system())
-        .add_system(snake_movement.system().label(SnakeMovement))
+        .add_system(snake_movement.system().label(SnakeMovementLabel))
+        .add_system(
+            gravity
+                .system()
+                .label(GravityLabel)
+                .after(SnakeMovementLabel),
+        )
         .add_system(
             gridlocation_to_transform
                 .system()
                 .label(TransformLabel)
-                .after(SnakeMovement),
+                .after(GravityLabel),
         )
         // gravity
         // gridlocation to transform
@@ -59,6 +73,7 @@ fn setup(
     commands.spawn_bundle(UiCameraBundle::default());
 
     let head_color = materials.add(Color::rgb(117.0 / 255.0, 167.0 / 255.0, 67.0 / 255.0).into());
+    let ground_color = materials.add(Color::rgb(136.0 / 255.0, 75.0 / 255.0, 43.0 / 255.0).into());
 
     *snake_parts = SnakeParts(vec![
         commands
@@ -69,6 +84,7 @@ fn setup(
                 ..Default::default()
             })
             .insert(GridLocation { x: 0, y: 0 })
+            .insert(Snake)
             .id(),
         commands
             .spawn()
@@ -78,6 +94,7 @@ fn setup(
                 ..Default::default()
             })
             .insert(GridLocation { x: 0, y: 0 })
+            .insert(Snake)
             .id(),
         commands
             .spawn()
@@ -87,6 +104,7 @@ fn setup(
                 ..Default::default()
             })
             .insert(GridLocation { x: 0, y: 0 })
+            .insert(Snake)
             .id(),
         commands
             .spawn()
@@ -96,16 +114,41 @@ fn setup(
                 ..Default::default()
             })
             .insert(GridLocation { x: 0, y: 0 })
+            .insert(Snake)
             .id(),
     ]);
 
-    // TODO: wire up parts later!
+    for i in -10..10 {
+        commands
+            .spawn()
+            .insert_bundle(SpriteBundle {
+                sprite: Sprite::new(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
+                material: ground_color.clone_weak(),
+                ..Default::default()
+            })
+            .insert(GridLocation { x: i, y: -1 })
+            .insert(Ground)
+            .id();
+    }
+
+    for i in 13..20 {
+        commands
+            .spawn()
+            .insert_bundle(SpriteBundle {
+                sprite: Sprite::new(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
+                material: ground_color.clone_weak(),
+                ..Default::default()
+            })
+            .insert(GridLocation { x: i, y: -1 })
+            .insert(Ground)
+            .id();
+    }
 }
 
 fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
     snake_parts: Res<SnakeParts>,
-    mut grid_locations: Query<&mut GridLocation>,
+    mut grid_locations: Query<&mut GridLocation, With<Snake>>,
 ) {
     let mut diff = GridLocation { x: 0, y: 0 };
     if keyboard_input.just_pressed(KeyCode::A) {
@@ -125,6 +168,8 @@ fn snake_movement(
         return;
     }
 
+    // TODO: check for blocked!
+
     for (prev, curr) in snake_parts.0.iter().zip(snake_parts.0[1..].iter()).rev() {
         if let Ok(prev_grid_location) = grid_locations.get_mut(*prev) {
             let tmp = prev_grid_location.clone();
@@ -139,6 +184,54 @@ fn snake_movement(
             *grid_location = grid_location.clone() + diff;
         }
     }
+}
+
+fn gravity(
+    snake_parts: Query<(&Snake, Entity)>,
+    ground: Query<(&Ground, Entity)>,
+    mut grid_locations: Query<&mut GridLocation>,
+) {
+    let ground_set = {
+        let mut tmp = HashSet::new();
+        for (_ground, e) in ground.iter() {
+            if let Ok(ground_grid_location) = grid_locations.get_mut(e) {
+                tmp.insert(ground_grid_location.clone());
+            }
+        }
+        tmp
+    };
+
+    dbg!(&ground_set);
+
+    let mut snake_fall = i32::MIN;
+
+    for (_snake, e) in snake_parts.iter() {
+        let snake_grid_location = grid_locations.get_mut(e).expect("snake grid location!");
+        let mut distance = -1;
+
+        while ground_set
+            .get(&GridLocation {
+                x: snake_grid_location.x,
+                y: snake_grid_location.y + distance,
+            })
+            .is_none()
+        {
+            distance -= 1;
+            if distance < -1000 {
+                break;
+            }
+        }
+        snake_fall = snake_fall.max(distance + 1);
+
+        dbg!(snake_fall, distance);
+    }
+
+    for (_snake, e) in snake_parts.iter() {
+        let mut snake_grid_location = grid_locations.get_mut(e).expect("snake grid location!");
+        snake_grid_location.y += snake_fall;
+    }
+
+    dbg!(snake_fall);
 }
 
 fn gridlocation_to_transform(mut q: Query<(&GridLocation, &mut Transform)>) {
