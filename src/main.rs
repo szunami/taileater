b65ca6c33d2b7ddedf71a04d3401f2339d7af7ec
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-19 18:25:51 -0400

    Basic workflow

--- src/main.rs
+++ src/main.rs
@@ -44,12 +44,16 @@ impl std::ops::Add for GridLocation {
     }
 }
 
+#[derive(Reflect, Default)]
+#[reflect(Component)]
 struct Snake;
 
 #[derive(Reflect, Default)]
 #[reflect(Component)]
 struct Ground;
 
+#[derive(Reflect, Default)]
+#[reflect(Component)]
 struct Food;
 
 struct SnakeParts(Vec<Entity>);
@@ -72,6 +76,8 @@ fn main() {
             .insert_resource(MyWorld(World::new(), TypeRegistry::default()))
             .register_type::<Ground>()
             .register_type::<GridLocation>()
+            .register_type::<Snake>()
+            .register_type::<Food>()
             .add_system(bevy::input::system::exit_on_esc_system.system())
             .add_startup_system(
                 (|world: &mut World| {
@@ -107,38 +113,39 @@ fn main() {
     } else {
         App::build()
             .add_plugins(DefaultPlugins)
-            .register_type::<core::option::Option<IndexFormat>>()
             .register_type::<Ground>()
             .register_type::<GridLocation>()
+            .register_type::<Snake>()
+            .register_type::<Food>()
             .insert_resource(SnakeParts(vec![]))
             .add_startup_system(setup.system().label(SetupLabel))
-            .add_system(cleanup.system().label(CleanupLabel).after(SetupLabel))
+            .add_system(cleanup.system())
             .add_system(bevy::input::system::exit_on_esc_system.system())
             .add_system(
                 (|q: Query<(&Ground, &Transform)>| {
                     for (_g, xform) in q.iter() {
-                        dbg!(xform);
+                        // dbg!(xform);
                     }
                 })
                 .system(),
             )
-            // .add_system(food.system().label(FoodLabel))
-            // .add_system(
-            //     snake_movement
-            //         .system()
-            //         .label(SnakeMovementLabel)
-            //         .after(FoodLabel),
-            // )
-            // .add_system(
-            //     gravity
-            //         .system()
-            //         .label(GravityLabel)
-            //         .after(SnakeMovementLabel),
-            // )
+            .add_system(food.system().label(FoodLabel))
+            .add_system(
+                snake_movement
+                    .system()
+                    .label(SnakeMovementLabel)
+                    .after(FoodLabel),
+            )
+            .add_system(
+                gravity
+                    .system()
+                    .label(GravityLabel)
+                    .after(SnakeMovementLabel),
+            )
             .add_system(
                 gridlocation_to_transform.system().label(TransformLabel), // .after(GravityLabel),
             )
-            // .add_system(win.system().label(WinLabel).after(GravityLabel))
+            .add_system(win.system().label(WinLabel).after(GravityLabel))
             // gravity
             // gridlocation to transform
             .run();
@@ -310,10 +317,13 @@ fn setup(
 fn cleanup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut snake_parts: ResMut<SnakeParts>,
 
-    q: Query<(&Ground, &GridLocation, Entity), Without<Sprite>>,
+    grounds: Query<(&Ground, &GridLocation, Entity), Without<Sprite>>,
+    snakes: Query<(&Snake, &GridLocation, Entity), Without<Sprite>>,
+    foods: Query<(&Food, &GridLocation, Entity), Without<Sprite>>,
 ) {
-    for (_ground, grid_location, e) in q.iter() {
+    for (_ground, grid_location, e) in grounds.iter() {
         commands.entity(e).insert_bundle(SpriteBundle {
             sprite: Sprite::new(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
             material: ground_color(&mut materials),
@@ -324,6 +334,55 @@ fn cleanup(
             )),
             ..Default::default()
         });
+    }
+    
+    for (_ground, grid_location, e) in foods.iter() {
+        commands.entity(e).insert_bundle(SpriteBundle {
+            sprite: Sprite::new(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
+            material: food_color(&mut materials),
+            transform: Transform::from_translation(Vec3::new(
+                grid_location.x as f32 * GRID_WIDTH,
+                grid_location.y as f32 * GRID_HEIGHT,
+                0.,
+            )),
+            ..Default::default()
+        });
+    }
+
+    // HACK: assume that the rightmost snake is first in the array
+    // TODO: use MapEntities
+    let mut internal_snake_parts = vec![];
+    let mut max_x = None;
+    for (_snake, grid_location, e) in snakes.iter() {
+        let id = commands
+            .entity(e)
+            .insert_bundle(SpriteBundle {
+                sprite: Sprite::new(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
+                material: snake_color(&mut materials),
+                transform: Transform::from_translation(Vec3::new(
+                    grid_location.x as f32 * GRID_WIDTH,
+                    grid_location.y as f32 * GRID_HEIGHT,
+                    0.,
+                )),
+                ..Default::default()
+            })
+            .id();
+        match max_x {
+            Some(max_x) => {
+                if grid_location.x > max_x {
+                    internal_snake_parts.insert(0, id)
+                } else {
+                    internal_snake_parts.push(id);
+                }
+            }
+            None => {
+                internal_snake_parts.push(id);
+                max_x = Some(grid_location.x)
+            }
+        }
+    }
+    if !internal_snake_parts.is_empty() {
+        *snake_parts = SnakeParts(internal_snake_parts);
     }
 }
 
@@ -467,10 +526,16 @@ fn food(
     snake_locations: Query<(&GridLocation, &Transform), (With<Snake>, Without<Food>)>,
     food_locations: Query<(&GridLocation, Entity), (With<Food>, Without<Snake>)>,
 ) {
-    let head_location = snake_locations
-        .get(*snake_parts.0.first().expect("head exists"))
-        .expect("head has grid location")
-        .0;
+    if snake_parts.0.is_empty() {
+        return;
+    }
+
+    let head_location = match snake_locations.get(*snake_parts.0.first().expect("head exists")) {
+        Ok(x) => x.0,
+        Err(_) => return,
+    };
+
+    // .0;
 
     let (tail_location, tail_xform) = snake_locations
         .get(*snake_parts.0.last().expect("tail exists"))
@@ -518,6 +583,10 @@ fn gridlocation_to_transform(time: Res<Time>, mut q: Query<(&GridLocation, &mut 
 }
 
 fn win(snake_parts: Res<SnakeParts>, snake_locations: Query<&GridLocation, With<Snake>>) {
+    if snake_parts.0.is_empty() {
+        return;
+    }
+
     if snake_parts.0.len() == 2 {
         return;
     }
@@ -623,7 +692,7 @@ fn my_cursor_system(
                 .insert(mouse_grid_location.clone())
                 .insert(Ground);
         }
-        
+
         if keyboard_input.just_pressed(KeyCode::S) {
             let mut q = my_world.0.query::<(&GridLocation, Entity)>();
 
@@ -659,7 +728,43 @@ fn my_cursor_system(
                 .insert(mouse_grid_location.clone())
                 .insert(Snake);
         }
-        
+
+        if keyboard_input.just_pressed(KeyCode::F) {
+            let mut q = my_world.0.query::<(&GridLocation, Entity)>();
+
+            let mut to_despawn = vec![];
+            for (grid_location, e) in q.iter(&my_world.0) {
+                if *grid_location == mouse_grid_location {
+                    to_despawn.push(e);
+                }
+            }
+            for e in to_despawn {
+                my_world.0.despawn(e);
+            }
+
+            for (grid_location, e) in grid_locations.iter() {
+                if *grid_location == mouse_grid_location {
+                    commands.entity(e).despawn_recursive();
+                }
+            }
+            commands
+                .spawn()
+                .insert_bundle(SpriteBundle {
+                    sprite: Sprite::new(Vec2::new(GRID_WIDTH, GRID_HEIGHT)),
+                    material: food_color(&mut materials),
+                    transform: mouse_xform,
+                    ..Default::default()
+                })
+                .insert(mouse_grid_location.clone())
+                .insert(Food);
+
+            my_world
+                .0
+                .spawn()
+                .insert(mouse_grid_location.clone())
+                .insert(Food);
+        }
+
         if keyboard_input.just_pressed(KeyCode::D) {
             let mut q = my_world.0.query::<(&GridLocation, Entity)>();
 
@@ -712,4 +817,8 @@ fn snake_color(mut materials: &mut ResMut<Assets<ColorMaterial>>) -> Handle<Colo
 
 fn cursor_color(mut materials: &mut ResMut<Assets<ColorMaterial>>) -> Handle<ColorMaterial> {
     materials.add(Color::rgb(164.0 / 255.0, 221.0 / 255.0, 219.0 / 255.0).into())
+}
+
+fn food_color(mut materials: &mut ResMut<Assets<ColorMaterial>>) -> Handle<ColorMaterial> {
+    materials.add(Color::rgb(165.0 / 255.0, 48.0 / 255.0, 48.0 / 255.0).into())
 }
