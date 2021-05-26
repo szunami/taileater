7a57217ac27ca19eadee81cc24357abc44307dd0
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-26 19:07:38 -0400

    No more cargo make

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 sudo apt install clang-12 --install-suggests
-cargo install --force cargo-make
-cargo make build-web
+# cargo install --force cargo-make
+# cargo make build-web
+cargo build --target wasm32-unknown-unknown --features web
 mkdir dist
 cp index.html dist/
 cp -r target dist/
