68193cc111f52130dea97c755c74d54748ba1d8b
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-26 21:20:37 -0400

    Force build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 cargo install wasm-pack
+cargo install -f wasm-bindgen-cli --version 0.2.69
 cargo install cargo-make
 cargo make build-web
 cargo build --target wasm32-unknown-unknown --features web
