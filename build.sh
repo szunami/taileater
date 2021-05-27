c6d82da411b14104545dd1f80fa17208512e856b
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-26 20:13:03 -0400

    Turn off fast builds

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cargo install wasm-pack
-# cargo install --force cargo-make
-# cargo make build-web
+cargo install  cargo-make
+cargo make build-web
 cargo build --target wasm32-unknown-unknown --features web
 mkdir dist
 cp index.html dist/
