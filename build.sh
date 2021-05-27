d06869b78c236329381f36f57e5659d72c6a5fb0
Sam Szuflita <sam.szuflita@gmail.com>
2021-05-26 23:42:25 -0400

    test

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 cargo install wasm-pack
 cargo install wasm-bindgen-cli --version 0.2.69
-# cargo install cargo-make
-# cargo make build-web
-cargo build --target wasm32-unknown-unknown --features web --release
+cargo install cargo-make
+cargo make --profile release build-web
+# cargo build --target wasm32-unknown-unknown --features web --release
 mkdir dist
 cp index.html dist/
 cp -r target dist/
